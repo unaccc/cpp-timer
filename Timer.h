98ad83de1ed2ea3913c906602be9eb74cf51f4b4
unac <119167100+unaccc@users.noreply.github.com>
2023-10-07 18:46:32 +0800

    Update Timer.h

--- Timer.h
+++ Timer.h
@@ -3,27 +3,22 @@
 #include <chrono>
 
 
-namespace Timer {
-    int begin_count = 0, end_count = 0;
-    std::chrono::time_point<std::chrono::system_clock> t1;
-    std::chrono::time_point<std::chrono::system_clock> t2;
-
-    inline void begin() {
-        begin_count++;
-        t1 = std::chrono::high_resolution_clock::now();
+class Timer{
+    std::chrono::time_point<std::chrono::system_clock> t1;      // begin
+    std::chrono::time_point<std::chrono::system_clock> t2;      // end
+public:
+    static Timer* begin() {
+        auto* timer = new Timer;
+        timer->t1 = std::chrono::system_clock::now();   // begin
+        return timer;
     }
 
-    inline void end() {
-        end_count++;
-        t2 = std::chrono::high_resolution_clock::now();
-    }
-
-    inline double result() {
-        if(begin_count != end_count)
-            printf("warning: begin count(%d) not equal end count(%d), please check your code!!!\n", begin_count, end_count);
-        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1);
+    double finish() {
+        this->t2 = std::chrono::system_clock::now();  // end
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1); // sub
         return double(duration.count()) * std::chrono::microseconds::period::num /
                std::chrono::microseconds::period::den;
     }
 
-}
+    ~Timer() = delete;
+};
